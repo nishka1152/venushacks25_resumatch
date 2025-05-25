b6f2853c73225487330e3901c8b852340c0f94ea
Nishka Jain <nishkaj@uci.edu>
2025-05-24 20:56:15 -0700

    some changes

--- resume_parser_ml_model.py
+++ resume_parser_ml_model.py
@@ -2,34 +2,11 @@ from resume_lines_and_labels import dataframe_builder
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
-sample_data = ['Bachelor of Science in Computer Science, UC Irvine', 
-               'Graduated with a GPA of 3.8/4.0', "Dean's List", 
-               'Honors', 
-               'Summa Cum Laude', 
-               'Magna Cum Laude', 
-               'Relevant Coursework: Data Structures, Algorithms, Machine Learning', 
-               'Software Engineering Intern at Google, Summer 2023',
-                'Developed internal tools using Python and Flask', 
-                'Collaborated with cross-functional teams to improve deployment pipelines', 
-                'Languages: Python, Java, JavaScript', 
-                'Frameworks: React, Flask, Node.js', 
-                'Tools: Git, Docker, Postman', 
-                'Built a resume parser using NLP and machine learning', 
-                'Created a full-stack task manager app using MERN stack', 
-                'Implemented sentiment analysis tool for Reddit comments',
-                'Can code in C++',
-                'Oversaw daily activity and outing planning for 100 clients',
-                'AWS Certified Solutions Architect – Associate', 
-                'Google IT Support Professional Certificate', 
-                'References available upon request', 
-                'Portfolio: www.myname.dev', 
-                'Love cooking', 
-                'Like rock climbing']
+from resume_parser import pdf_to_python_list
 
-<<<<<<< HEAD
 def section_generator():
     sample_data = ['Bachelor of Science in Computer Science, UC Irvine', 
-                   'BS in Early Childhood Development (1999)', 'BA in Elementary Education',
+                   'BS ', 'BA in Elementary Education',
                 'Graduated with a GPA of 3.8/4.0', "Dean's List", 
                 'Honors', 
                 'Summa Cum Laude', 
@@ -54,26 +31,29 @@ def section_generator():
                     'Like rock climbing']
 
     test_data = dataframe_builder(sample_data)
-=======
-test_data = dataframe_builder(sample_data)
->>>>>>> 57351e2c0303c84dc19124e8749150de0fe8ed95
 
 
 
-texts = list(test_data["text"])
-labels = list(test_data["label"])
+    texts = list(test_data["text"])
+    labels = list(test_data["label"])
 
-vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(texts)
-print(vectorizer.get_feature_names_out())
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(texts)
+    #print(vectorizer.get_feature_names_out())
 
-model = LogisticRegression()
-model.fit(X, labels)
+    model = LogisticRegression()
+    model.fit(X, labels)
+    pdf_path_test = '/Users/nishkajain/Desktop/functionalsample.pdf'
+    resume_lines = pdf_to_python_list(pdf_path_test)
+    sections = {"Education": [], "Experience": [], "Other": [], "Skills": [], "Projects": [], "Certifications": []}
+    for i in resume_lines:
+        # print(i)
+        X_new = vectorizer.transform([i])
 
-new_line = "Assisted families of special needs clients with researching financial assistance and healthcare"
-X_new = vectorizer.transform([new_line])
+        pred = model.predict(X_new)[0]
+        # print("Predicted Section:", pred)
+        sections[pred].append(i)
 
-<<<<<<< HEAD
     return sections
     
 print(section_generator()["Other"])
@@ -82,7 +62,3 @@ print(section_generator()["Other"])
 
     
     
-=======
-pred = model.predict(X_new)[0]
-print("Predicted Section:", pred)
->>>>>>> 57351e2c0303c84dc19124e8749150de0fe8ed95
